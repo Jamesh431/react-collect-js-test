3164c18b0d41d1212b3f460be6685385512bbc9c
james <jdhales2686@gmail.com>
2024-08-30 15:24:01 -0600

    added ts configs and libraries for linter

--- src/main.tsx
+++ src/main.tsx
@@ -4,3 +4,22 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Checkout from "./routes/Checkout";
 import Home from "./routes/Home";
+import App from "./routes/App";
+
+export const routeArr = [
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+
+      {
+        path: "checkout/",
+        element: <Checkout />,
+      },
+    ],
+  },
+];
