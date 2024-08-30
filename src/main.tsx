1593d80b30000b19184892ab2a0ded35ced2cc91
james <jdhales2686@gmail.com>
2024-08-30 14:55:51 -0600

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Checkout from "./routes/Checkout";
+import Home from "./routes/Home";
